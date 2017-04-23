4b873a7610ef0b31fc5ff8ba536c64ca56f3427a
jihadLkmaty218 <ahgjihadmeords19771@gmail.com>
2017-04-23 17:48:51 +0200

    Add files via upload

---
+++ Gihad3.0.sh
@@ -0,0 +1,235 @@
+#!/bin/bash
+
+clear
+#DEFINED COLOR SETTINGS
+RED=$(tput setaf 2 && tput bold)
+GREEN=$(tput setaf 2 && tput bold)
+STAND=$(tput sgr0)
+BLUE=$(tput setaf 2 && tput bold)
+
+
+cat << !
+                          ########                  #
+                      #################            #
+                   ######################         #
+                  #########################      #
+                ############################
+               ##############################
+               ###############################
+              ###############################
+              ##############################
+                              #    ########   #
+                 ##        ###        ####   ##
+                                      ###   ###
+                                    ####   ###
+               ####          ##########   ####
+               #######################   ####
+                 ####################   ####
+                  ##################  ####
+                    ############      ##
+                       ########        ###
+                      #########        #####
+                    ############      ######
+                   ########      #########
+                     #####       ########
+                       ###       #########
+                      ######    ############
+                     #######################
+                     #   #   ###  #   #   ##
+                     ########################
+                      ##     ##   ##     ##                                                     
+!
+sleep 1
+
+echo ""
+echo ""
+echo ""
+echo $RED"  ________  ___  ___  ___  ________  ________    _______   _____  ________  "   
+echo $RED" |\   ____\|\  \|\  \|\  \|\   __  \|\   ___ \  /  ___  \ / __  \|\   __  \  "  
+echo $RED" \ \  \___|\ \  \ \  \\\  \ \  \|\  \ \  \_|\ \/__/|_/  /|\/_|\  \ \  \|\  \  " 
+echo $RED"  \ \  \  __\ \  \ \   __  \ \   __  \ \  \ \\ \__|//  / ||/ \ \  \ \   __  \  "
+echo $RED"   \ \  \|\  \ \  \ \  \ \  \ \  \ \  \ \  \_\\ \  /  /_/__   \ \  \ \  \|\  \  "
+echo $RED"    \ \_______\ \__\ \__\ \__\ \__\ \__\ \_______\|\________\  \ \__\ \_______\  "
+echo $RED"     \|_______|\|__|\|__|\|__|\|__|\|__|\|_______| \|_______|   \|__|\|_______|   "
+echo ""                                                                                                                                                       
+echo ""
+echo $BLUE"     ﺔﻤﺷﺎﻐﻟ​ﺍ ﺓﻮﻘﻟ​ﺍ ﺔﻤﺷﺎﻐﻟ​ﺍ ﺓﻮﻘﻟ​ﺍ ﺔﻤﺷﺎﻐﻟ​ﺍ ﺓﻮﻘﻟ​ﺍ ﺔﻤﺷﺎﻐﻟ​ﺍ ﺓﻮﻘﻟ​ﺍ ﺔﻤﺷﺎﻐﻟ​ﺍ ﺓﻮﻘﻟ​ﺍ"
+echo ""
+echo $BLUE"      Github ﻰﻠﻋ ﻲﺑﺎﺴﺣ >>   https//www.Github.com/jihadLkmaty218/ "
+sleep 2
+clear
+
+echo ""
+echo ""
+echo $RED"              ██████ ██████╗██████╗ ██████╗██████╗██████╗███╗     "
+echo $RED"              ╚════╝╚════╝ ╚════╝ ╚════╝ ╚════╝╚════╝╚════╝       "
+echo $RED"           ██████                                            ██████ "
+echo $RED"           ╚════╝         Gihad Alkmaty from Libya           ╚════╝ "
+echo $RED"           ██████                                            ██████ "
+echo $RED"           ╚════╝         tool  Version 3.0  SANNER          ╚════╝ " 
+echo $RED"           ██████                                            ██████ "                                          
+echo $RED"           ╚════╝                                            ╚════╝ "
+echo $RED"           ██████               ^________^                   ██████ "
+echo $RED"           ╚════╝              | Libya218 |                  ╚════╝ "
+echo $RED"           ██████          <)  |____----__|                  ██████ "
+echo $RED"           ╚════╝        <-----------||-----                 ╚════╝ "                           
+echo $RED"           ██████             =      ||                      ██████ "
+echo $RED"           ██████            =       ||-                     ██████ "
+echo $RED"           ╚════╝          =         ||- -                   ╚════╝ "
+echo $RED"           ██████         =          || - -                  ██████ "
+echo $RED"           ╚════╝        =           ||  - -                 ╚════╝ "
+echo $RED"           ██████        =                - -                ██████ "
+echo $RED"           ╚════╝                           =                ╚════╝ "
+echo $RED"           ██████ ██████ ██████╗██████╗ ██████╗██████╗██████╗███╗ "
+echo $RED"           ╚════╝╚════╝╚════╝╚════╝ ╚════╝ ╚════╝ ╚════╝╚════╝╚═╝ "
+echo ""   
+echo ""
+echo $BLUE"                      3.0ﺖﻟ​ﺎﺜﻟ​ﺍ ﺭﺍﺪﺻﻻ​ﺍ ﻲﻃﺎﻤﻘﻟ​ﺍ ﺩﺎﻬﺟ ﺓﺍﺩﺍ"
+echo ""
+echo $BLUE"          facebook ﻰﻠﻋ ﻲﺑﺎﺴﺣ >> https://www.facebook.com/Gihad.Metasploit  "
+sleep 4
+clear
+
+echo ""
+echo ""
+echo $RED"       ▄██████▄   ▄█     ▄█    █▄       ▄████████ ████████▄  "
+echo $RED"      ███    ███ ███    ███    ███     ███    ███ ███   ▀███ "
+echo $RED"      ███    █▀  ███▌   ███    ███     ███    ███ ███    ███ "
+echo $RED"     ▄███        ███▌  ▄███▄▄▄▄███▄▄   ███    ███ ███    ███ "
+echo $RED"    ▀▀███ ████▄  ███▌ ▀▀███▀▀▀▀███▀  ▀███████████ ███    ███ "
+echo $RED"      ███    ███ ███    ███    ███     ███    ███ ███    ███ "
+echo $RED"      ███    ███ ███    ███    ███     ███    ███ ███   ▄███ "
+echo $RED"      ████████▀  █▀     ███    █▀      ███    █▀  ████████▀  "
+echo ""                                                         
+echo ""   
+echo ""
+echo $BLUE"     facebook       ttps://www.facebook.com/Gihad.Metasploit "
+echo ""
+echo $BLUE"      Github        https//www.Github.com/jihadLkmaty218/ "
+sleep 2
+clear
+
+echo ""
+echo $RED"       ▄██████▄   ▄█     ▄█    █▄       ▄████████ ████████▄  "
+echo $RED"      ███    ███ ███    ███    ███     ███    ███ ███   ▀███ "
+echo $RED"      ███    █▀  ███▌   ███    ███     ███    ███ ███    ███ "
+echo $RED"     ▄███        ███▌  ▄███▄▄▄▄███▄▄   ███    ███ ███    ███ "
+echo $RED"    ▀▀███ ████▄  ███▌ ▀▀███▀▀▀▀███▀  ▀███████████ ███    ███ "
+echo $RED"      ███    ███ ███    ███    ███     ███    ███ ███    ███ "
+echo $RED"      ███    ███ ███    ███    ███     ███    ███ ███   ▄███ "
+echo $RED"      ████████▀  █▀     ███    █▀      ███    █▀  ████████▀  "
+echo $RED"                ======================================="; 
+echo $RED"                =    1.  whois lookup                 =";
+echo $RED"                =        whoisﻲﺑ ﻊﻗﺍﻮﻣ ﺺﺤﻓ ﻲﻟ         =";
+echo $RED"                =        whois < 扫描仪在网站上       =";
+echo $RED"                =  ---------------------------------- =";
+echo $RED"                =    2.  SCAN On website tool uniscan =";
+echo $RED"                =     uniscan ﺓﺍﺩﺍ ﻲﺑ ﻊﻗﺍﻮﻤﻟ​ﺍ ﺺﺤﻓ ﻲﻟ  =";   
+echo $RED"                = ------------------------------------=";
+echo $RED"                =    3.  SCAN IP > Nmap               =";
+echo $RED"                =        NMAP ﺓﺩﺎﺑ ﺓﺰﻬﺟﻻ​ﺍ ﺺﺤﻓ ﻲﻟ      =";
+echo $RED"                =        圣IP NMAP                    =";
+echo $RED"                =------------------------------------ =";
+echo $RED"                =    4.  DNS tool dnsenum             =";
+echo $RED"                =        dnsenumﺓﺩﺎﺑ ﻊﻗﺍﻮﻤﻟ​ﺍ ﺺﺤﻓ ﻲﻟ   =";
+echo $RED"                =-------------------------------------=";
+echo $RED"                =    5.  wordlist Crunch              =";
+echo $RED"                =  crunch ﺓﺩﺎﺑ  ﺕﺍﺩﺭﻮﺴﺑ ﺔﺘﺴﻟ ﻦﻳﻮﻜﺗ ﻲﻟ =";
+echo $RED"                =-------------------------------------=";
+echo $RED"                =                                     ="; 
+echo $RED"                =                                     ="; 
+echo $RED"                =    8.  EXIT                         ="; 
+echo $RED"                ======================================";
+echo $RED"       █████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗╗█████╗ "      
+echo $RED"       ╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝  "    
+echo $RED"       █████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗  "
+echo $RED"        ════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝  "
+echo $BLUE"                           Tool GIhad ALkmaty "$STAND  
+read menuoption
+
+if [ $menuoption = "1" ]; then
+echo "site ?"
+read target
+whois $target
+echo ""
+read -p "ENTER "
+./Gihad3.0.sh
+else
+
+
+if [ $menuoption = "2" ]; then
+echo "site ? ﻊﻗﻮﻤﻟ​ﺍ"
+read target
+dig $target any
+echo ""
+echo ""
+uniscan -u $target -qweds
+echo ""
+read -p "ENTER"
+./Gihad3.0.sh
+else
+
+if [ $menuoption = "3" ]; then
+echo "Ip"
+read target
+echo ""
+echo $STAND" ENTER  "
+read nmap;
+echo ""
+echo ""
+nmap -v $target
+/Gihad3.0.sh
+else
+
+if [ $menuoption = "4" ]; then
+echo " site ? ﻊﻗﻮﻤﻟ​ﺍ"
+read target
+echo ""
+cd /usr/bin
+dnsenum --enum -f /usr/share/dnsenum/dns.txt --update a -r $target
+echo ""
+read -p "ENTER"
+/Gihad3.0.sh
+else
+
+if [ $menuoption = "5" ]; then
+echo ""
+read target
+echo ""
+cd /usr/bin
+crunch 1 100 $target 
+echo ""
+read -p "ENTER"
+/Gihad3.0.sh
+else
+
+<<COMMENT1
+ﻝ​ﺎﻐﺷ ﺓﺍﺩﻻ​ﺍ ﻞﻀﻔﺗ ﻥﺎﺸﻋ ﻲﺸﺑ ﺚﺒﻌﺗﻻ ﻒﻠﻤﻟ​ﺍ ﺢﺘﻔﻳ ﻦﻣ ﻞﻜﻟ ﺃﻮﻔﻋ
+ﻙﺪﻨﻋ
+COMMENT1
+
+if [ $menuoption = "8" ]; then
+exit
+fi
+fi
+fi
+fi
+fi
+fi
+fi
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
